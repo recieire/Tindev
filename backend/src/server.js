ac02e07751ca76208795a491a34b9d1117a109e7
Recieire Carbelim <recieiremarcos@gmail.com>
2021-05-18 01:59:16 -0300

    added connection to mongo db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0.azpdl.mongodb.net/tindevDb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 server.use(express.json());
 
