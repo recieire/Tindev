f1d451de23c24db6efb590dcdead077b03c7eff6
Recieire Carbelim <recieiremarcos@gmail.com>
2021-05-19 03:21:03 -0300

    added controller

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 
 
 module.exports = {
-  store(req, res) {
+  async store(req, res) {
     const { username } = req.body;
 
-    const response = axios.get(`https://api.github.com/users/${username}`)
-    console.log(response.data)
-    return res.json({ok: true});
+    const response = await axios.get(`https://api.github.com/users/${username}`)
+
+    return res.json(response.data);
   }
 };
