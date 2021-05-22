ac28ae1e4cf69aa1238171e1596e242fc5411c92
Recieire Carbelim <recieiremarcos@gmail.com>
2021-05-22 11:59:19 -0300

    added dislike and resolved list of devs filter

--- backend/src/controllers/DislikeController.js
+++ backend/src/controllers/DislikeController.js
@@ -14,11 +14,8 @@ if (!targetDev) {
   return res.status(400).json({ error: 'Dev does not exist'});
 }
 
-if (targetDev.likes.includes(loggedDev._id)) {
-  console.log('DEU MATCH');
-}
 
-loggedDev.likes.push(targetDev._id);
+loggedDev.dislikes.push(targetDev._id);
 await loggedDev.save();
 
 return res.json(loggedDev);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const server = express();
+
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0.azpdl.mongodb.net/tindevDb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
+server.use(cors());//permitir acesso a requisição externa do frontend/mobile
 server.use(express.json());
 
 server.use(routes);
