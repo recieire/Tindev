e4a337fc17f237f4fcda206eb1d058ce9ff7da10
Recieire Carbelim <recieiremarcos@gmail.com>
2021-05-23 11:43:19 -0300

    added call to backend api|added style to Main.js

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,80 @@
 import React from 'react';
+import './Main.css';
 
-export default function Main() {
-  return <h1>Hello World</h1>
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Main({ match }) {
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        <li>
+          <img src="https://avatars.githubusercontent.com/u/38233554?v=4" alt="" />
+          <footer>
+            <strong>Recieire</strong>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni accusamus, asperiores, voluptas sunt aperiam, porro eveniet consectetur officia distinctio nisi ea quos qui nesciunt alias odit consequatur molestias veritatis. Magnam?</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars.githubusercontent.com/u/38233554?v=4" alt="" />
+          <footer>
+            <strong>Recieire</strong>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni accusamus, asperiores, voluptas sunt aperiam, porro eveniet consectetur officia distinctio nisi ea quos qui nesciunt alias odit consequatur molestias veritatis. Magnam?</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars.githubusercontent.com/u/38233554?v=4" alt="" />
+          <footer>
+            <strong>Recieire</strong>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni accusamus, asperiores, voluptas sunt aperiam, porro eveniet consectetur officia distinctio nisi ea quos qui nesciunt alias odit consequatur molestias veritatis. Magnam?</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars.githubusercontent.com/u/38233554?v=4" alt="" />
+          <footer>
+            <strong>Recieire</strong>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni accusamus, asperiores, voluptas sunt aperiam, porro eveniet consectetur officia distinctio nisi ea quos qui nesciunt alias odit consequatur molestias veritatis. Magnam?</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
